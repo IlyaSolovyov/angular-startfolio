1db149c177e8144ead4c66af3ece3c77b281d308
Baghin Denis <b.denio34@gmail.com>
2017-12-04 03:36:09 +0200

    [Client] Added person component

---
+++ StartFolio/ClientApp/app/components/person/person.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component, Output } from '@angular/core';
+
+@Component({
+    selector: 'my-person',
+    styleUrls: ['./person.component.css'],
+    templateUrl: './person.component.html'
+})
+export class PersonComponent {
+    public title = "Вердикт";
+    public backgroundColorClass = "material-indigo";
+    public personImgUrl = "dist//SO.jpg";
+    public personImgAlt = "alternative content"; 
+    public opinion = "Persona 5 — пожалуй, лучшая часть не только в серии Persona, но и в серии Shin Megami Tensei в целом. И одна из основных претенденток на игру года, сколь бы нишевой она ни казалась. Абсолютный must have для любителей жанра.";
+    public Name = "Папа";
+    public Surname = "Пежа";
+    public Age = "99";
+    public WhoIsThis = "Папа Пежа";
+}
