d7324d01e7228fb1b7e318e1def768de9e4bfb74
Baghin Denis <b.denio34@gmail.com>
2017-12-04 03:36:32 +0200

    [Client] Added product component

---
+++ StartFolio/ClientApp/app/components/product/product.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component, Output } from '@angular/core';
+
+@Component({
+    selector: 'my-product',
+    styleUrls: ['./product.component.css'],
+    templateUrl: './product.component.html'
+})
+export class ProductComponent {
+    public title = "Денчик";
+    public backgroundColorClass = "material-red";
+    public productImgUrl = "dist//BA.jpg";
+    public productImgAlt = "alternative content";
+    public mainText = "Persona 5 — пожалуй, лучшая часть не только в серии Persona, но и в серии Shin Megami Tensei в целом. И одна из основных претенденток на игру года, сколь бы нишевой она ни казалась. Абсолютный must have для любителей жанра.";
+    public subText = "Persona 5 — пожалуй, лучшая часть не только в серии Persona, но и в серии Shin Megami Tensei в целом. И одна из основных претенденток на игру года, сколь бы нишевой она ни казалась. Абсолютный must have для любителей жанра.";
+}
