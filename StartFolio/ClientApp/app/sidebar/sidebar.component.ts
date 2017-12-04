fd5f13b72a26b1232c438ef26f934dd546f39a52
Baghin Denis <b.denio34@gmail.com>
2017-12-04 03:37:17 +0200

    [Client] Added files to create sidebar component

---
+++ StartFolio/ClientApp/app/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+}
