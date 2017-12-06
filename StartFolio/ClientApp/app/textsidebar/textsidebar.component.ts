061c74fb86956c5e62659626713a62e509bb1fe3
IlyaSolovyov <solovyov909@gmail.com>
2017-12-06 23:45:33 +0200

    removed removal

---
+++ StartFolio/ClientApp/app/textsidebar/textsidebar.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-textsidebar',
+    templateUrl: './textsidebar.component.html',
+    styleUrls: ['./textsidebar.component.css']
+})
+export class TextSidebarComponent {
+    private _opened: boolean = true;
+
+    private _toggleSidebar() {
+        this._opened = !this._opened;
+    }
+}
