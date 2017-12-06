234437a7cfde60a344d5b5812d778718db7e0696
Baghin Denis <b.denio34@gmail.com>
2017-12-07 00:28:09 +0200

    [Client] Add icons and edit button component

---
+++ StartFolio/ClientApp/app/components/editbtn/editbtn.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component, Output } from '@angular/core';
+
+@Component({
+    selector: 'edit-btn',
+    styleUrls: ['./editbtn.component.css'],
+    templateUrl: './editbtn.component.html'
+})
+export class EditButtonComponent {
+}
