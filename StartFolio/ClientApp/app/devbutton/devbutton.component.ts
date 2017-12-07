9f1c072729afe397f1cf89fccca6c8269cd1859c
Baghin Denis <b.denio34@gmail.com>
2017-12-07 09:36:05 +0200

    [Client] Fixed bug with devbutton and scroll

--- StartFolio/ClientApp/app/devbutton/devbutton.component.ts
+++ StartFolio/ClientApp/app/devbutton/devbutton.component.ts
@@ -1,15 +1,35 @@
 ﻿import { Component, HostListener, Directive } from '@angular/core';
 
+
+
 @Component({
     selector: 'devbutton',
     styleUrls: ['./devbutton.component.css'],
     templateUrl: './devbutton.component.html'
 })
 export class DevButtonComponent {
-    @HostListener("window:scroll", ["$event"])
+    @HostListener("window:scroll", [])
+    onScroll(): void {
+        let devButton = document.getElementById("devButton");
+        if (devButton != null) {
+            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                devButton.classList.remove("hidden-devBtn");
+                devButton.classList.add("visible-devBtn");
+            } else {
+                devButton.classList.remove("visible-devBtn");
+                devButton.classList.add("hidden-devBtn");
+            }
+        }
+    }
+    /*@HostListener("window:scroll", ["$event"])
     onWindowScroll() {
+        
         //In chrome and some browser scroll is given to body tag
-        let pos = (document.documentElement.scrollTop || document.body.scrollTop) + 662;
+        
+
+        let magicOffset = 662;
+
+        let pos = (document.documentElement.scrollTop || document.body.scrollTop) + magicOffset;
         let bottom = document.documentElement.offsetHeight;
 
         let devButton = document.getElementById("devButton");
@@ -22,6 +42,5 @@ export class DevButtonComponent {
                 devButton.classList.remove("visible-devBtn");
                 devButton.classList.add("hidden-devBtn");
             }
-        }
-    }
-}
+        }*/
+ }
