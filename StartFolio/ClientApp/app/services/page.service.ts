7d1a9f1b2e45c5523291cdc6c969b35f4a926abf
Baghin Denis <b.denio34@gmail.com>
2017-12-13 08:36:00 +0200

    [Services] Made page.service.ts(don't work)

---
+++ StartFolio/ClientApp/app/services/page.service.ts
@@ -0,0 +1,19 @@
+﻿import { Injectable } from '@angular/core';
+import { Http } from '@angular/http'
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class PageService {
+
+    constructor(private http: Http) { }
+
+    getPage(id: number) {
+        return this.http.get(`api/Page/${id}`)
+            .map(response => response.json());
+    }
+
+    updatePage() {
+
+    }
+
+}
