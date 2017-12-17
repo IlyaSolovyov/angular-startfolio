fdffc30d19de43a3d84d8542f97bdf40784560d4
Baghin Denis <b.denio34@gmail.com>
2017-12-17 16:51:47 +0200

    [ClientModel] Attached data bindings to product sidebar form and added model

--- StartFolio/ClientApp/app/components/sidebars/productsidebar/productsidebar.component.ts
+++ StartFolio/ClientApp/app/components/sidebars/productsidebar/productsidebar.component.ts
@@ -1,8 +1,33 @@
 ﻿import { Component } from '@angular/core';
+import { Page } from '../../../page';
+import { PageService } from '../../../services/page.service';
 
 @Component({
     selector: 'my-productsidebar',
     templateUrl: './productsidebar.component.html',
+    providers: [PageService]
 })
 export class ProductSidebarComponent {
+
+    constructor(public page: Page, public pageService: PageService) {
+
+    }
+
+    editProductInfo(ev) {
+        this.page.details = JSON.stringify(this.model);
+        this.pageService.updateDetails(this.page.position, this.page.details);
+    }
+
+    model = {
+        title: '',
+        mainText: '',
+        subText: '',
+        photo: {},
+        backgroundColor: ''
+    }
+
+    updateImage(ev) {
+        var image = ev.target.files[0];
+        this.model.photo = image;
+    }
 }
