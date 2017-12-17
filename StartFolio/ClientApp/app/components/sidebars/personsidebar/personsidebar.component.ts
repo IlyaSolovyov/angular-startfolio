a467927f1639af3800e9518ce89c91e27b31fa57
Baghin Denis <b.denio34@gmail.com>
2017-12-17 16:51:05 +0200

    [ClientModel] Attached data binding to person sidebar form and added model

--- StartFolio/ClientApp/app/components/sidebars/personsidebar/personsidebar.component.ts
+++ StartFolio/ClientApp/app/components/sidebars/personsidebar/personsidebar.component.ts
@@ -1,8 +1,35 @@
 ﻿import { Component } from '@angular/core';
+import { Page } from '../../../page';
+import { PageService } from '../../../services/page.service';
 
 @Component({
     selector: 'my-personsidebar',
     templateUrl: './personsidebar.component.html',
+    providers: [PageService]
 })
 export class PersonSidebarComponent {
+
+    constructor(public page: Page, public pageService: PageService) {
+
+    }
+
+    editPersonInfo(ev) {
+        this.page.details = JSON.stringify(this.model);
+        this.pageService.updateDetails(this.page.position, this.page.details);
+    }
+
+    model = {
+        title: '',
+        mainText: '',
+        personName: '',
+        age: 0,
+        position: '',
+        photo: {},
+        backgroundColor: ''        
+    }
+
+    updateImage(ev) {
+        var image = ev.target.files[0];
+        this.model.photo = image;
+    }
 }
