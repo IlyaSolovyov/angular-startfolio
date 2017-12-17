abc904007e703316ac31d066440646df944271fa
Baghin Denis <b.denio34@gmail.com>
2017-12-17 16:50:18 +0200

    [ClientModel] Attached data bindings to gallery sidebar form

--- StartFolio/ClientApp/app/components/sidebars/gallerysidebar/gallerysidebar.component.ts
+++ StartFolio/ClientApp/app/components/sidebars/gallerysidebar/gallerysidebar.component.ts
@@ -1,8 +1,31 @@
 ﻿import { Component } from '@angular/core';
+import { PageService } from '../../../services/page.service';
+import { Page } from '../../../page';
 
 @Component({
     selector: 'my-gallerysidebar',
     templateUrl: './gallerysidebar.component.html',
+    providers: [PageService]
 })
 export class GallerySidebarComponent {
+
+    constructor(public page: Page, public pageService: PageService) {
+
+    }
+
+    editGalleryInfo(ev) {
+        this.page.details = JSON.stringify(this.model);
+        this.pageService.updateDetails(this.page.position, this.page.details);
+    }
+
+    model = {
+        title: '',
+        backgroundColor: '',
+        photos: []
+    }
+
+    updateImage(ev, index) {
+        var image = ev.target.files[0];
+        this.model.photos[index] = image;
+    }
 }
