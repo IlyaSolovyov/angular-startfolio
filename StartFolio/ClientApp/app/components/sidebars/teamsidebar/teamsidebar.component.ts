971d7bc1220d692816a45c4a7bec820847526486
Baghin Denis <b.denio34@gmail.com>
2017-12-17 16:53:01 +0200

    [ClientModel] Attached data bindings to team sidebar form and added model

--- StartFolio/ClientApp/app/components/sidebars/teamsidebar/teamsidebar.component.ts
+++ StartFolio/ClientApp/app/components/sidebars/teamsidebar/teamsidebar.component.ts
@@ -1,8 +1,49 @@
 ﻿import { Component } from '@angular/core';
+import { Page } from '../../../page';
+import { PageService } from '../../../services/page.service';
 
 @Component({
     selector: 'my-teamsidebar',
     templateUrl: './teamsidebar.component.html',
+    providers: [PageService]
 })
 export class TeamSidebarComponent {
+
+    constructor(public page: Page, public pageService: PageService) {
+
+    }
+
+    editTextInfo(ev) {
+        this.page.details = JSON.stringify(this.model);
+        this.pageService.updateDetails(this.page.position, this.page.details);
+    }
+
+    model = {
+        title: '',
+        mainText: '',
+        backgroundColor: '',
+
+        teammate1_Name: '',
+        teammate1_Description: '',
+        teammate1_Link: '',
+        teammate1_Photo: {},
+
+        teammate2_Name: '',
+        teammate2_Description: '',
+        teammate2_Link: '',
+        teammate2_Photo: {},
+    }
+
+    updateImage(ev, index) {
+        var image = ev.target.files[0];
+
+        switch (index) {
+            case 1:
+                this.model.teammate1_Photo = image;
+                break;
+            case 2:
+                this.model.teammate2_Photo = image;
+                break;
+        }
+    }
 }
