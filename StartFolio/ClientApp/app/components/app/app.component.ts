cd6a7730e8fccb821414adcaabce54e69737073b
Baghin Denis <b.denio34@gmail.com>
2017-12-17 16:33:34 +0200

    [ClientModel] Made a blank according to client model

--- StartFolio/ClientApp/app/components/app/app.component.ts
+++ StartFolio/ClientApp/app/components/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component, HostListener, Directive } from '@angular/core';
+import { Component, HostListener, Directive, OnInit } from '@angular/core';
+import { PageService } from '../../services/page.service';
+import { Page } from '../../page';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    providers: [PageService]
 })
-export class AppComponent {
-    
+export class AppComponent implements OnInit {
+    pages: Observable<Page[]>;
+
+    constructor(public pageService: PageService) {
+
+    }
+
+    ngOnInit() {
+        this.pages = this.pageService.getPages();
+    }
+
+
 }
