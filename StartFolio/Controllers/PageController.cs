594f8a31adc018d717867cd051d8205263e44246
IlyaSolovyov <solovyov909@gmail.com>
2017-12-19 15:41:05 +0200

    Fixed server "Update Details" method not accepting files.

--- StartFolio/Controllers/PageController.cs
+++ StartFolio/Controllers/PageController.cs
@@ -85,8 +85,22 @@ namespace StartFolio.Controllers
 
         // PUT: api/Page/5/Details
         [HttpPut("{position}/Details")]
-        public async Task<IActionResult> UpdateDetailsAsync(int position, [FromForm] string details)
+        public async Task<IActionResult> UpdateDetailsAsync(int position, [FromForm] string details, IFormFileCollection uploads)
         {
+            foreach (var uploadedFile in uploads)
+            {
+                string oldFilename = uploadedFile.FileName;
+                string newFilename = Guid.NewGuid().ToString();
+                details = details.Replace(oldFilename, newFilename);
+                // путь к папке Files             
+                string path = "/Images/" + newFilename;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+            }
+
             Page page = await pageRepository.GetPage(position);
             await pageRepository.UpdatePageDetails(page.Id, details);
             return Ok();
