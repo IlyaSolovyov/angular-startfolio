4b61c30d21bfe19e154b7191f97f66e8815b277e
Baghin Denis <b.denio34@gmail.com>
2017-12-19 03:31:23 +0200

    [ClientModel] There are 3 hardcoded objects in gallery now

--- StartFolio/ClientApp/app/components/gallery/gallery.component.ts
+++ StartFolio/ClientApp/app/components/gallery/gallery.component.ts
@@ -11,12 +11,16 @@ export class GalleryComponent  implements OnInit{
     model = {
         title:           'Галерея продуктов',
         backgroundColor: 'material-blue',
-        ImgUrl:          'Images//BA.jpg',
-        sometext:        'Some text about project'
+        ImgUrl1:         'Images//BA.jpg',
+        ImgUrl2:         'Images//SO.jpg',
+        ImgUrl3:         'Images//SO.jpg',
+        description1:    'BA text about project',
+        description2:    'SO text about project',
+        description3:    'SoBa text about project'
     };
     
     getBackgroundColor() {
-      return this.model.backgroundColor;
+        return this.model.backgroundColor;
     }
 
     ngOnInit()
@@ -26,8 +30,12 @@ export class GalleryComponent  implements OnInit{
             let details: Details = JSON.parse(this._page.details);
             this.model.title = details.title;
             this.model.backgroundColor = details.backgroundColor;
-            this.model.ImgUrl = details.imgUrl;
-            this.model.sometext = details.description;
+            this.model.ImgUrl1 = details.imgUrl1;
+            this.model.ImgUrl2 = details.imgUrl2;
+            this.model.ImgUrl3 = details.imgUrl3;
+            this.model.description1 = details.description1;
+            this.model.description2 = details.description2;
+            this.model.description3 = details.description3;
         }       
     }
 
@@ -37,6 +45,10 @@ interface Details
 {
     title: string;
     backgroundColor: string;
-    imgUrl: string;
-    description: string;
+    imgUrl1: string;
+    imgUrl2: string;
+    imgUrl3: string;
+    description1: string;
+    description2: string;
+    description3: string;
 }
