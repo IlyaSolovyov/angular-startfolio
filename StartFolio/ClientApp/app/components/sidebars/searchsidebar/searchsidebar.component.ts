a398f07e792ddff0fb22391a20c986f3093d8c77
IlyaSolovyov <solovyov909@gmail.com>
2017-12-20 22:10:22 +0200

    Added CORS support, again.

--- StartFolio/ClientApp/app/components/sidebars/searchsidebar/searchsidebar.component.ts
+++ StartFolio/ClientApp/app/components/sidebars/searchsidebar/searchsidebar.component.ts
@@ -145,6 +145,6 @@ export class SearchSidebarComponent implements OnInit {
         }
 
         var page = new Page(position, template, JSON.stringify(details));
-      //  this.pageService.addPage(page);
+        this.pageService.addPage(page);
     }
 }
