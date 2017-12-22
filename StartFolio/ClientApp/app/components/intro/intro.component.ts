66a8cd1ceef43836ec355a715791da39aff3b00c
Baghin Denis <b.denio34@gmail.com>
2017-12-22 02:52:40 +0200

    [Client] Added snackbar call after page edit and changes submit

---
+++ StartFolio/ClientApp/app/components/intro/intro.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component, Output, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'intro',
+    styleUrls: ['./intro.component.css'],
+    templateUrl: './intro.component.html'
+})
+export class IntroComponent {
+    model: Details = {
+        title:              'Ваш сайт пока еще пуст!',
+        mainText:           'Добавьте контент на сайт с помощью кнопки "Добавить" в нижнем правом углу. Выберите компонент, который вам приглянулся и редактируйте его содержимое, как вашей душе угодно!',
+        backgroundColor:    '#0E9D57'
+    }
+
+    getBackgroundColor() {
+        return this.model.backgroundColor;
+    }
+}
+interface Details {
+    title:              string;
+    mainText:           string;
+    backgroundColor:    string;
+}
